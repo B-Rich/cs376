938fbe9ce52844a5190a6b80e3e7f74c7a5996ca
Maurizio Calo Caligaris <mauriziocalo07@gmail.com>
2013-06-08 07:04:45 -0700

    nevermind

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,5 @@ EvernoteSample::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  ENV['EN_CONSUMER_KEY']= "mauriziocalo";
-  ENV['EN_CONSUMER_SECRET'] = "406c3d9577c1de59"
+
 end
