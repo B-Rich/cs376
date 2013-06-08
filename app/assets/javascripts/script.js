971c4616de162da8242ade7c872ae71b72fcf223
Maurizio Calo Caligaris <mauriziocalo07@gmail.com>
2013-06-08 12:52:56 -0700

    added new files

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,230 @@
+//Constants
+var width = Math.min(window.screen.availWidth, 1000); //320
+var height = Math.min(window.screen.availHeight, 1400); //356
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var formh = height-margin.top-margin.bottom;
+// var formw = Math.min(width-margin.right-margin.left, 400);
+var formw = width-margin.right-margin.left;
+var submith = 36 + 10;
+var texth = 96;
+var totalh = texth + submith;
+var cdiameter = 70;
+var radius = Math.max(width, height);
+var color = d3.scale.category20c();
+//var tags = ["polarbear dog", "lion turtle", "turtle duck", "eel hound", "koala otter"];
+var tags = [];
+$.get('api/listNotebooks',function(data){
+	data = htmlDecode(data)
+	var arr = JSON.parse(data.trim());
+	for (var i=0;i<arr.length;i++){
+		tags.push(arr[i].name)
+	}
+});
+
+
+var drag = d3.behavior.drag()
+	.origin(Object)
+	.on("drag", dragmove)
+	.on("dragend", dragend);
+var arc = d3.svg.arc()
+    .outerRadius(radius)
+    .innerRadius(80);
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return 1; });
+
+//Setup
+d3.select("svg")
+	.attr("width", width)
+	.attr("height", height);
+d3.select("#container")
+	.style("width", function(){return width + "px";})
+	.style("height", function(){return height + "px";});
+d3.select("rect")
+	.attr("x", margin.left)
+	.attr("y", height/2 - totalh/2)
+	.attr("width", formw)
+	.attr("height", texth)
+	.attr("fill", "grey")
+	.attr("rx", 10)
+	.attr("ry", 10)
+	.attr("id", "draggable");	
+d3.select("#formobject")
+	.attr("x", margin.left)
+	.attr("y", height/2 - totalh/2)
+	.attr("width", formw)
+	.attr("height", 356);
+d3.select("#note")
+	.style("height", function(){return texth + "px";})
+	.style("width", function(){return formw-6 + "px";});
+	
+	
+//Variables
+var note; //the text area content
+//var tag; //the selected tag
+var dragdx = 0;
+var dragdy = 0;
+
+function handleClick(event){
+	note = document.getElementById("note").value;
+	console.log(note);
+    transition();	
+    return false;
+}
+
+function transition(){
+	
+	//Resize, fade, and remove form
+    d3.select("#note")
+		.transition()
+		.duration(500)
+			.style("border-radius", "80px")
+			.style("background", "grey")
+			.style("opacity", 0);
+	d3.select("#submit")
+		.transition()
+		.duration(500)
+			.style("opacity", 0);
+	d3.select("#formobject")
+		.transition()
+		.delay(500)
+		.remove();
+		
+	//Tween hidden rectangle to circle
+	var rect = d3.select("rect")
+		.data([{x:(width/2) - (cdiameter/2), y:(height/2) - (cdiameter/2)}])
+		.attr("rx", 60)
+		.attr("ry", 60)
+		.call(drag);
+	rect.transition()
+		.delay(100)
+		.duration(1000)
+			.attr("width", cdiameter)
+			.attr("height", cdiameter)			
+			.attr("x", (width/2) - (cdiameter/2))
+			.attr("y", (height/2) - (cdiameter/2));
+	
+	//Generate Sunburst
+	var svg = d3.select("svg")
+		.insert("g", "rect")
+			.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+	var g = svg.selectAll(".sunburst")
+		.data(pie(tags))
+    .enter().append("g")
+		.style("opacity", 0);
+	g.append("path")
+		.attr("class", "arc")
+		.attr("d", arc)
+		.style("fill", function(d, i) { return color(i); });
+	g.append("text")
+		.attr("transform", function(d) { 
+			var angle = ((180*(d.startAngle+d.endAngle)/2)/Math.PI)-90;
+			var newy = arc.centroid(d)[1]/2;
+			var newx = arc.centroid(d)[0]/2;
+			return "translate("+[newx, newy]+")rotate("+angle+")rotate("+(angle>90?-180:0)+")"; 
+		})
+		.attr("text-anchor", "start")
+		.attr("dy", "-.2em")
+		.style("text-anchor", "middle")
+		.text(function(d) { return d.data; });
+	g.transition()
+		.delay(1500)
+		.duration(1000)
+		.style("opacity", 100);			
+}
+
+function dragmove(d) {
+	dragdx += d3.event.dx;
+	dragdy += d3.event.dy;
+	d3.select(this)
+		.attr("x", d.x = Math.max(0, Math.min(width - cdiameter, d3.event.x)))
+		.attr("y", d.y = Math.max(0, Math.min(height - cdiameter, d3.event.y)));
+}
+
+function dragend(d) {
+	var direction = Math.min(Math.abs(dragdx), Math.abs(dragdy));
+	d3.select(this).transition()
+		.duration(3000)
+		.ease("cubic-out")
+		// .attr("x", d.x += Math.min((width/8) * (dragdx/direction), 0))
+		// .attr("y", d.y += Math.min((height/8) * (dragdy/direction), 0));
+		.attr("x", d.x += 0)
+		.attr("y", d.y += 0);
+	dragdx = 0;
+	dragdy = 0;
+
+	var tag = d3.select(document.elementFromPoint(d.y, d.x));
+	var svg = d3.select("svg");
+		
+	// Remove the rectangle
+	var rect = d3.select("#draggable");
+	rect.transition()
+		.duration(1000)
+			.style("opacity", 0);
+		
+	// Get the element position and detect the arc underneath
+	var x = event.clientX ? event.clientX : event.changedTouches[0].clientX;
+	var y = event.clientY ? event.clientY : event.changedTouches[0].clientY;
+	
+	
+	
+
+	setTimeout(function (){
+		rect.remove();
+		var elementMouseIsOver = document.elementFromPoint(x, y),
+			patharc, tag;	
+		patharc = d3.select(elementMouseIsOver)[0][0].__data__;
+		if(patharc){
+			tag = patharc.data;
+			console.log(tag)
+			//Post data to server to save note
+			$.get('/api/createNote', { title:'New AJAX Note', content:note, notebookName:tag } );
+
+		}else{
+			alert("no tag selected");
+		}
+		//Fade out and fade in feedback
+		fadeoutall();
+		feedback(tag);
+    }, 1100);
+}
+
+function fadeoutall() {
+	var svg = d3.selectAll("g")
+		.transition()
+		.delay(0)
+		.duration(2000)
+			.style("opacity", 0);
+	svg.transition().delay(2000)
+		.remove();
+	d3.select("svg")
+		.transition().delay(2000)
+		.remove();
+	
+}
+
+function feedback(tag) {
+	setTimeout(function (){
+        var container = d3.select("#container");
+		var div = container.append("div")
+			.style("color", "white")
+			.attr("class", "feedback")
+			.style("margin-top", function(){return (height/2)-100 + "px";})
+			.style("width", function(){return Math.max(width/4, 200) + "px";});
+		div.append("p")
+			.text("New note created: \""+note+"\" and tagged \""+tag+"\"");
+		
+
+		div.append("a")
+			.attr("href", "/")
+			.attr("class", "again")
+			.text("New Note");
+    }, 2000);
+
+}
+
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes[0].nodeValue;
+}
---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,87 @@
+class ApiController < ApplicationController
+	def createNote
+		#TODO: Include photo and video. Document parameters
+		client = EvernoteOAuth::Client.new(token: authtoken)
+	    note_store = client.note_store
+	    title = params[:title] ? params[:title] : "New Note"
+	    content = params[:content] ? params[:content] : ""
+		tagNames = params[:tagNames] ? params[:tagNames].split(',') : []
+		notebookName = params[:notebookName] ? params[:notebookName] : ""
+		notebooks = note_store.listNotebooks
+		notebookGuid = defaultNotebookGuid(notebooks)
+		notebooks.each do |notebook|
+			if notebook.name == notebookName
+				notebookGuid = notebook.guid
+			end
+		end
+
+		#begin
+			note = Evernote::EDAM::Type::Note.new(
+			  title: title,
+			  tagNames: tagNames,
+			  content: '<?xml version="1.0" encoding="UTF-8"?>'+'<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'+'<en-note>'+content+'</en-note>',
+			  notebookGuid: notebookGuid 
+			)
+			created_note = note_store.createNote(authtoken, note)
+		#rescue Evernote::EDAM::Error::EDAMUserException => e
+		#	render(:partial=>"display_error", :locals=>{:error_message=>translate_error(e)})	
+		#end
+	end
+
+	#Returns JSON array of notebook names
+	def listNotebooks
+		client = EvernoteOAuth::Client.new(token: authtoken)
+	    note_store = client.note_store
+    	@notebooks = note_store.listNotebooks
+
+	end
+
+	#Stub method:To all notes containing tagX, assign them tagY. Optional replace parameter
+
+	#Helper methods (put it in helpers folder but didn't work for some reason.)
+	def defaultNotebookGuid(notebooks)
+		notebooks.each do |notebook|
+			if notebook.defaultNotebook?
+				return notebook.guid 
+			end
+		end
+		return -1
+	end
+
+
+	def translate_error(e)
+		error_name = "unknown"
+		case e.errorCode
+		when Evernote::EDAM::Error::EDAMErrorCode::AUTH_EXPIRED
+		  error_name = "AUTH_EXPIRED"
+		when Evernote::EDAM::Error::EDAMErrorCode::BAD_DATA_FORMAT
+		  error_name = "BAD_DATA_FORMAT"
+		when Evernote::EDAM::Error::EDAMErrorCode::DATA_CONFLICT
+		  error_name = "DATA_CONFLICT"
+		when Evernote::EDAM::Error::EDAMErrorCode::DATA_REQUIRED
+		  error_name = "DATA_REQUIRED"
+		when Evernote::EDAM::Error::EDAMErrorCode::ENML_VALIDATION
+		  error_name = "ENML_VALIDATION"
+		when Evernote::EDAM::Error::EDAMErrorCode::INTERNAL_ERROR
+		  error_name = "INTERNAL_ERROR"
+		when Evernote::EDAM::Error::EDAMErrorCode::INVALID_AUTH
+		  error_name = "INVALID_AUTH"
+		when Evernote::EDAM::Error::EDAMErrorCode::LIMIT_REACHED
+		  error_name = "LIMIT_REACHED"
+		when Evernote::EDAM::Error::EDAMErrorCode::PERMISSION_DENIED
+		  error_name = "PERMISSION_DENIED"
+		when Evernote::EDAM::Error::EDAMErrorCode::QUOTA_REACHED
+		  error_name = "QUOTA_REACHED"
+		when Evernote::EDAM::Error::EDAMErrorCode::SHARD_UNAVAILABLE
+		  error_name = "SHARD_UNAVAILABLE"
+		when Evernote::EDAM::Error::EDAMErrorCode::UNKNOWN
+		  error_name = "UNKNOWN"
+		when Evernote::EDAM::Error::EDAMErrorCode::VALID_VALUES
+		  error_name = "VALID_VALUES"
+		when Evernote::EDAM::Error::EDAMErrorCode::VALUE_MAP
+		  error_name = "VALUE_MAP"
+		end
+		rv = "Error code was: #{error_name}[#{e.errorCode}] and parameter: [#{e.parameter}]"  
+	end
+
+end
