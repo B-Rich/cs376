9f2527a2592891b4322dde9766ca7ba1eea728fe
Maurizio Calo Caligaris <mauriziocalo07@gmail.com>
2013-06-08 06:44:22 -0700

    for some reason heroku can't load evernote.yml config file (I think) so I'm just hardcoding manually settings (e.g. api keys, sandbox, etc

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,8 @@ config = YAML.load(ERB.new(File.read('config/evernote.yml')).result)[Rails.env]
 site = 'https://www.evernote.com'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :evernote, config['consumer_key'], config['consumer_secret'], :client_options => {:site => site}
+  #provider :evernote, mauriziocalo, config['consumer_secret'], :client_options => {:site => site}
+  provider :evernote, mauriziocalo, "406c3d9577c1de59", :client_options => {:site => site}
 end
 
 OmniAuth.config.on_failure = LoginController.action(:oauth_failure)
